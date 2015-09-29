421ff0623c1004d170be844d22cd80fa87647bd0
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2015-09-29 13:33:53 +0300

    Event handlers added

--- MyPad/Dialogs/Syndication/EntriesListDialog.Designer.cs
+++ MyPad/Dialogs/Syndication/EntriesListDialog.Designer.cs
@@ -8,6 +8,13 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        Panel panel1;
+        Label label1;
+        ListView listView1;
+        Button createNewEntry;
+        Button editExistingEntry;
+        Button deleteEntry;
+        Button saveButton;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -33,6 +40,101 @@
             var resources = new System.ComponentModel.ComponentResourceManager(this.GetType());
             Debug.Assert (resources != null);
             this.SuspendLayout();
+
+            panel1 = new Panel ();
+            label1 = new Label ();
+            listView1 = new ListView ();
+            createNewEntry = new Button ();
+            editExistingEntry = new Button ();
+            deleteEntry = new Button ();
+            saveButton = new Button ();
+
+            panel1.Controls.Add (label1);
+            panel1.Controls.Add (listView1);
+            panel1.Controls.Add (createNewEntry);
+            panel1.Controls.Add (editExistingEntry);
+            panel1.Controls.Add (deleteEntry);
+            panel1.Controls.Add (saveButton);
+
+            this.Controls.Add (panel1);
+
+            panel1.Dock = DockStyle.Fill;
+
+            int formSpacing = 6;
+
+            label1.Top = formSpacing;
+            label1.Left = formSpacing;
+            label1.AutoSize = true;
+            label1.Text = "News list:";
+            label1.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+
+            int topGuide = label1.Top + label1.Height + formSpacing;
+
+            createNewEntry.Height = 20;
+            createNewEntry.Top = topGuide;
+            createNewEntry.Width = 80;
+            createNewEntry.Left = createNewEntry.Parent.Width - createNewEntry.Width - formSpacing;
+            createNewEntry.Text = "Create";
+            createNewEntry.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            editExistingEntry.Height = 20;
+            editExistingEntry.Top = createNewEntry.Top + createNewEntry.Height + formSpacing;
+            editExistingEntry.Width = 80;
+            editExistingEntry.Left = editExistingEntry.Parent.Width - editExistingEntry.Width - formSpacing;
+            editExistingEntry.Text = "Edit";
+            editExistingEntry.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            deleteEntry.Height = 20;
+            deleteEntry.Top = editExistingEntry.Top + editExistingEntry.Height + formSpacing;
+            deleteEntry.Width = 80;
+            deleteEntry.Left = deleteEntry.Parent.Width - deleteEntry.Width - formSpacing;
+            deleteEntry.Text = "Delete";
+            deleteEntry.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            int maxWidth = Math.Max (Math.Max (createNewEntry.Width, editExistingEntry.Width), deleteEntry.Width);
+
+            saveButton.Height = 20;
+            saveButton.Top = saveButton.Parent.Height - saveButton.Height - formSpacing;
+            saveButton.Width = 80;
+            saveButton.Left = saveButton.Parent.Width - saveButton.Width - formSpacing;
+            saveButton.Text = "Save";
+            saveButton.Anchor = AnchorStyles.Right | AnchorStyles.Bottom;
+
+            int bottomGuide = saveButton.Top - formSpacing;
+
+            listView1.Top = topGuide;
+            listView1.Height = bottomGuide - listView1.Top;
+            listView1.Left = formSpacing;
+            listView1.Width = listView1.Parent.Width - 3 * formSpacing - maxWidth;
+            listView1.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
+            listView1.View = View.Details;
+            listView1.Columns.Add("Index", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("URL", -2, HorizontalAlignment.Left);
+            for (int i = 0; i < 5; ++i)
+            {
+                var it = new ListViewItem (i.ToString ());
+                it.Tag = i;
+                it.SubItems.Add ("item text " + i.ToString());
+                listView1.Items.Add (it);
+            }
+            listView1.FullRowSelect = true;
+            listView1.HideSelection = false;
+            listView1.Items[0].Selected = true;
+            listView1.ClientSizeChanged += (object sender, EventArgs e) => {
+                int total = 0;
+                ColumnHeader last = null;
+                foreach (ColumnHeader c in listView1.Columns)
+                    {
+                    c.AutoResize (ColumnHeaderAutoResizeStyle.ColumnContent);
+                    //c.Width = -1;
+                    total += c.Width;
+                    last = c;
+                    }
+                if (last != null)
+                    {
+                    total -= last.Width;
+                    last.Width = listView1.ClientSize.Width - total;
+                    }
+            };
+
+
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable; // FixedDialog
             this.Name = "EntriesListDialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -40,6 +142,11 @@
             this.TransparencyKey = System.Drawing.Color.Fuchsia;
             this.ResumeLayout(false);
             this.PerformLayout();
+
+            createNewEntry.Click += CreateNewEntry_Click;;
+            editExistingEntry.Click += EditExistingEntry_Click;
+            deleteEntry.Click += DeleteEntry_Click;
+            saveButton.Click += SaveButton_Click;
         }
 
         #endregion
--- MyPad/Dialogs/Syndication/EntriesListDialog.cs
+++ MyPad/Dialogs/Syndication/EntriesListDialog.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Windows.Forms;
+    using System.Collections.Generic;
     using System.ServiceModel.Syndication;
 
     partial class EntriesListDialog : Form
@@ -9,6 +10,39 @@
         public EntriesListDialog ()
         {
             InitializeComponent();
+        }
+
+        void CreateNewEntry_Click (object sender, EventArgs e)
+        {
+            var dlg = new EntryCreateOrEditDialog ();
+            dlg.ShowDialog ();
+        }
+
+        void EditExistingEntry_Click (object sender, EventArgs e)
+        {
+            var dlg = new EntryCreateOrEditDialog ();
+            dlg.ShowDialog ();
+        }
+
+        void DeleteEntry_Click (object sender, EventArgs e)
+        {
+            var itemsToDelete = new  List<ListViewItem> ();
+            // message box
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.Selected)
+                {
+                    itemsToDelete.Add (item);
+                }
+            }
+            foreach (var item in itemsToDelete)
+            {
+                listView1.Items.Remove (item);
+            }
+        }
+
+        void SaveButton_Click (object sender, EventArgs e)
+        {
         }
     }
 }
